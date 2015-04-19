ea9261891a27674db98380f3184cadb10f7cd287
Elliott Stoneham <elliott.stoneham@gmail.com>
2015-04-19 17:24:54 +0100

    Use Haxe as travis-ci language

---
+++ goinstall.sh
@@ -0,0 +1,17 @@
+# extract from: https://gist.github.com/einthusan/f72c6dc7e0eff88b8bfe
+
+echo "Download and install Go, as well as create GOPATH directory"
+cd ~
+wget https://storage.googleapis.com/golang/go1.4.1.linux-amd64.tar.gz 
+tar -xf go1.4.1.linux-amd64.tar.gz && rm go1.4.1.linux-amd64.tar.gz
+mv go /usr/local && mkdir -p /usr/local/gopath 
+echo "set enviornment variables required for Go"
+export GOROOT=/usr/local/go
+export GOPATH=/usr/local/gopath
+cat <<EOF >> ~/.bashrc
+export GOROOT=/usr/local/go
+export GOPATH=/usr/local/gopath
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+export GORACE=log_path=$GOPATH/racereport
+export w=$GOPATH/src/github.com
+EOF
