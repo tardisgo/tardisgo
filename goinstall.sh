104080a8504d829ebf98def4b8852311a524f25e
Elliott Stoneham <elliott.stoneham@gmail.com>
2015-04-19 18:06:02 +0100

    travis-ci change location of GOPATH

--- goinstall.sh
+++ goinstall.sh
@@ -4,13 +4,13 @@ echo "Download and install Go, as well as create GOPATH directory"
 cd ~
 wget https://storage.googleapis.com/golang/go1.4.1.linux-amd64.tar.gz 
 tar -xf go1.4.1.linux-amd64.tar.gz && rm go1.4.1.linux-amd64.tar.gz
-sudo mv go /usr/local && sudo mkdir -p /usr/local/gopath 
+sudo mv go /usr/local && sudo mkdir -p ~/gopath 
 echo "set enviornment variables required for Go"
 export GOROOT=/usr/local/go
-export GOPATH=/usr/local/gopath
+export GOPATH=~/gopath
 cat <<EOF >> ~/.bashrc
 export GOROOT=/usr/local/go
-export GOPATH=/usr/local/gopath
+export GOPATH=~/gopath
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 export GORACE=log_path=$GOPATH/racereport
 export w=$GOPATH/src/github.com
