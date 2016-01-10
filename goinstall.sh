512b1762cb9cfecc49c8626e939982bff81dd558
Elliott Stoneham <elliott.stoneham@gmail.com>
2016-01-10 17:17:49 +0000

    CI install go in /usr/lib

--- goinstall.sh
+++ goinstall.sh
@@ -4,12 +4,12 @@ echo "Download and install Go, as well as create GOPATH directory"
 cd ~
 wget https://storage.googleapis.com/golang/go1.5.2.linux-amd64.tar.gz 
 tar -xf go1.5.2.linux-amd64.tar.gz && rm go1.5.2.linux-amd64.tar.gz
-sudo mv go /usr/local && sudo mkdir -p ~/gopath 
+sudo mv go /usr/lib && sudo mkdir -p ~/gopath 
 echo "set enviornment variables required for Go"
-export GOROOT=/usr/local/go
+export GOROOT=/usr/lib/go
 export GOPATH=~/gopath
 cat <<EOF >> ~/.bashrc
-export GOROOT=/usr/local/go
+export GOROOT=/usr/lib/go
 export GOPATH=~/gopath
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 export GORACE=log_path=$GOPATH/racereport
